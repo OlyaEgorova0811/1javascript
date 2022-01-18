f7a76accfbd327a5214a20a130bdbe2f14028af8
Olya Egorova <olyaegorova@MacBook-Pro-Olya.local>
2022-01-18 18:50:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let namber = 4;
+const persone = "Alex";
+
+const bool = false;
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+
+let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+console.log(arr[1]);
+
+const result = confirm("Are you here?");
+
+const category = "toys";
+
+const user = "Ivan";
+alert(`привет, ${user}`);
+
+let incr = 10,
+  decr = 10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
